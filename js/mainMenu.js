57e8291dc05cdf8f54cdc706051febb9384e81a8
Yago <yago.pereira@estudante.ufjf.br>
2023-04-27 04:11:12 -0300

    seção contato

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -3,9 +3,9 @@ const menuItems = document.querySelectorAll('.menu__principal_link');
 menuItems.forEach(elemento => elemento.innerHTML += '<span class="efeito"></span>');
 
 const linkIntro = document.querySelector('.tree');
-
-linkIntro.innerHTML += '<span class="efeito__linha"></span>';
-
 const linkTCC = document.querySelector('.tcc__link');
+const arvLink = document.querySelector('.arv__link');
 
-linkTCC.innerHTML += '<span class="efeito__linha"></span>';
+const conjuntoDeLinks = [linkIntro, linkTCC, arvLink];
+
+conjuntoDeLinks.forEach(link => link.innerHTML += '<span class="efeito__linha"></span>');
